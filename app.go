a406385adced0ba89bbda3b97632c4184a8263bb
liujie <liujie@tech.youyuan.com>
2016-02-19 15:19:59 +0800

    修改bug

--- app.go
+++ app.go
@@ -11,7 +11,7 @@ import (
 	"path/filepath"
 	"sync"
 	"errors"
-	"github.com/liujianping/wechat/entry"
+	"github.com/liujieking/wechat/entry"
 )
 
 type CallbackInterface interface {
--- callback.go
+++ callback.go
@@ -1,7 +1,7 @@
 package wechat
 
 import (
-	"github.com/liujianping/wechat/entry"
+	"github.com/liujieking/wechat/entry"
 )
 
 type Callback struct{
